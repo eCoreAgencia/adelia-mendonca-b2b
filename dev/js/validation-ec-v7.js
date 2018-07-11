cfeb340b795860ee6471c1e33b1a664a48e1a926
Sergio <sergio.henrique@ecorecomunicacao.com>
2018-07-11 10:01:22 -0300

    macbook paula
    pc paula

--- dev/js/validation-ec-v7.js
+++ dev/js/validation-ec-v7.js
@@ -6,14 +6,14 @@ if(window.location.href.indexOf("sc")!=-1){
 
 $(document).ready(function(){
 	if(document.location.href.indexOf('/?sc=2') > -1 ) {
-		$('.menu-departamento a, a.product-image, .header-qd-v1-user-links a, #logo a .prateleira .shelf-qd-v1-image-link, h3.shelf-qd-v1-product-name a, .footer-qd-v1-links a').each(function(){
+		$('.menu-departamento a, a.product-image, .header-qd-v1-user-links a, .header-qd-v1-logo a, .prateleira .shelf-qd-v1-image-link, h3.shelf-qd-v1-product-name a, .footer-qd-v1-links a').each(function(){
 			var myLink = $(this).attr('href');
 			var changeMyLink = $(this).attr('href', myLink+'?sc=2');
 		});
 	}
 	
 	if(document.location.href.indexOf('/?sc=3') > -1 ) {
-		$('.menu-departamento a, a.product-image, .header-qd-v1-user-links a, #logo a .prateleira .shelf-qd-v1-image-link, h3.shelf-qd-v1-product-name a, .footer-qd-v1-links a').each(function(){
+		$('.menu-departamento a, a.product-image, .header-qd-v1-user-links a, .header-qd-v1-logo a, .prateleira .shelf-qd-v1-image-link, h3.shelf-qd-v1-product-name a, .footer-qd-v1-links a').each(function(){
 			var myLink = $(this).attr('href');
 			var changeMyLink = $(this).attr('href', myLink+'?sc=3');
 		});
